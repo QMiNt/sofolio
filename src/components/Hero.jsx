d11d060ea30a38990e9a0e9469d6d66ae3f81c27
Aditya Agarwal <99405991+QMiNt@users.noreply.github.com>
2023-07-15 16:43:20 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div>
+<spline-viewer url="https://prod.spline.design/dgPXjohh-ldWz4iJ/scene.splinecode"></spline-viewer>
+        </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Tech.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  return (
+    
+<spline-viewer url="https://prod.spline.design/J13QoGasa2LJaUhR/scene.splinecode"></spline-viewer>
+ 
+  );
+};
+
+export default Tech;
